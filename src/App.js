aa4ea2b226522fffa1c8f004492616c3cc1d9fb9
waldganger <antony.merle@yahoo.fr>
2021-09-18 17:17:05 +0200

    add title and prepare floor asset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import statue from "./assets/statue.png"
+import floor from "./assets/floor.png"
 import { Aurelius } from "./Aurelius";
 function App() {
   return (
     <div className="container">
-      
-      <img src={statue} className="statue"/>
+      <div className="title">
+      <p >Aurelius <br/>meditations</p>
+      </div>
+      <div className="meditations"><img src={statue} className="statue"/>
+      {/* <img src={floor} className="floor"/> */}
       <Aurelius />
     </div>
+      </div>
   );
 }
 
